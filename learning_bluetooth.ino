e4262a0c5b74c5efb763e0a50015382e982ab662
joshpartal <joshua_partal25@milton.edu>
2025-02-14 13:54:55 -0500

    added a bit more

--- learning_bluetooth.ino
+++ learning_bluetooth.ino
@@ -5,13 +5,32 @@ int const TX_PIN = 10; //this in the tx pin, this transmits the bluetooth
 SoftwareSerial tooth(TX_PIN, RX_PIN); // make a bluetooth object 
 //set tx and rx pins
 //tx goes first, then rx
+char davis;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  tooth.begin(9600);
 
 }
+//comment
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-}
+    if(tooth.available() >0){
+      davis = tooth.read();
+      tooth.print("reading new input: ");
+      tooth.println(davis);
+    }
+
+    if(davis == 'd'){
+      Serial.println("davis has value!");
+     
+    }
+     Serial.println(davis);
+    delay(100);
+
+  }
+
+
